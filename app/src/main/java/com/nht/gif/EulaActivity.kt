d6536b85f890ee9ed41653e77483a9148db26181
Tuan Nguyen <tuancoltech@gmail.com>
2026-05-21 15:47:02 +0700

    [Compose] Migrate EulaActivity from XML -> Jetpack Compose

--- app/src/main/java/com/nht/gif/EulaActivity.kt
+++ app/src/main/java/com/nht/gif/EulaActivity.kt
@@ -3,37 +3,32 @@ package com.nht.gif
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.nht.gif.databinding.ActivityEulaBinding
-import com.nht.gif.toolbox.Toolbox.onClick
+import com.nht.gif.ui.EulaScreen
+import com.nht.gif.ui.theme.EasyGifTheme
 
 class EulaActivity : AppCompatActivity() {
-  private val binding by lazy { ActivityEulaBinding.inflate(layoutInflater) }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(binding.root)
-    binding.mtvVersionInfo.text = getString(R.string.version_X, BuildConfig.VERSION_NAME)
-    if (MySettings.eulaAccepted) {
-      binding.mbDisagree.visibility = View.GONE
-      binding.mbAgree.text = getString(R.string.close)
-      binding.mtvPleaseAccept.text = getString(R.string.you_have_read_agreed_eula_to_withdraw)
-      binding.mbAgree.onClick(HapticFeedbackType.CONFIRM) {
-        finish()
-      }
-    } else {
-      binding.mbAgree.onClick(HapticFeedbackType.CONFIRM) {
-        MySettings.eulaAccepted = true
-        MainActivity.start(this@EulaActivity)
-        finish()
+    val eulaAlreadyAccepted = MySettings.eulaAccepted
+    setContent {
+      EasyGifTheme {
+        EulaScreen(
+          versionName = BuildConfig.VERSION_NAME,
+          eulaAlreadyAccepted = eulaAlreadyAccepted,
+          onAgree = {
+            if (!eulaAlreadyAccepted) {
+              MySettings.eulaAccepted = true
+              MainActivity.start(this@EulaActivity)
+            }
+            finish()
+          },
+          onDisagree = ::finish,
+        )
       }
     }
-    binding.mbDisagree.onClick {
-      finish()
-    }
-
-
   }
 
 
